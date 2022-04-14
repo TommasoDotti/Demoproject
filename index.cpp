acc857165e304b510dbd62a85673f788a246418c
Tommaso Dotti <tommasodotti@MacBook-Air-di-Tommaso.local>
2022-04-14 14:42:08 +0200

    first commit

---
+++ index.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int int1, int2, int3;
+	printf("enter first\n");
+	scanf("%d", &int1);
+	printf("enter second\n");
+	scanf("%d", &int2);
+	printf("\nla somma è %d \n",int1+int2);
+	return 0;
+
+}
